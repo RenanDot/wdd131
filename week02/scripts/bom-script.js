63874b2b1432068b010b642ae0d4f7dabe07c19c
RenanDot <rcampos1@byupathway.edu>
2025-07-24 09:46:50 -0300

    debugging activity

--- week02/scripts/bom-script.js
+++ week02/scripts/bom-script.js
@@ -2,16 +2,6 @@ const input = document.querySelector('#favchap');
 const addButton = document.querySelector('button');
 const list = document.querySelector('#list');
 
-const li = document.createElement('li');
-li.textContent = input.value;
-
-const deleteButton = document.createElement('button');
-deleteButton.textContent = '❌';
-
-li.append(deleteButton);
-
-list.append(li);
-
 addButton.addEventListener('click', function() {
     if (input.value.trim() !== ''){
         const li = document.createElement('li');
@@ -26,6 +16,12 @@ addButton.addEventListener('click', function() {
 
         input.value = '';
         input.focus();
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
     }
 });
 
