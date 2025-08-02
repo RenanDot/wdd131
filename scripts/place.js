2aa30838a1ae15f80323e1c4387d412261a5fb18
RenanDot <rcampos1@byupathway.edu>
2025-08-02 13:33:16 -0300

    changing the js

--- scripts/place.js
+++ scripts/place.js
@@ -1,14 +1,15 @@
 const temperature = document.getElementById("temperature");
 const wind = document.getElementById("wind");
 
-function calculateWindChill(temp, windSpeed) {
-    if (temp <= 10 && windSpeed > 4.8) {
-        return Math.round(13.12 + 0.6215 * temp - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temp * Math.pow(windSpeed, 0.16)) + "°C";
-    } else {
-        return "N/A";
-    }
-}
+const calculateWindChill = (t, w) => Math.round(13.12 + 0.6215 * t - 11.37 * w ** 0.16 + 0.3965 * t * w ** 0.16) + "°C";
 
 const windChill = document.getElementById("windChill");
 
-windChill.textContent = calculateWindChill(parseFloat(temperature.textContent), parseFloat(wind.textContent));
+if (parseFloat(temperature.textContent) <= 10 && parseFloat(wind.textContent) > 4.8) {
+    if (temperature && wind) {
+        windChill.textContent = calculateWindChill(parseFloat(temperature.textContent), parseFloat(wind.textContent));
+    }
+} 
+else {
+    windChill.textContent = "N/A";
+}
