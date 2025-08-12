fe40c72f799d179405ed81ae82c32abf21208f4d
RenanDot <rcampos1@byupathway.edu>
2025-08-12 10:28:47 -0300

    other change

--- scripts/review.js
+++ scripts/review.js
@@ -2,7 +2,6 @@ let reviewCount = localStorage.getItem("reviewCount");
 reviewCount = reviewCount ? parseInt(reviewCount) + 1 : 1;
 localStorage.setItem("reviewCount", reviewCount);
 
-// Update the review counter element if it exists
 const spanCount = document.getElementById("review-counter");
 if (spanCount) {
     spanCount.textContent = reviewCount;
